b2fa123c0885a46b12d8730c7add5706e181d9b6
Saeedzxz123 <saeedbh1190@gmail.com>
2025-12-23 09:07:19 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,161 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [money, setMoney] = useState(100);
+  const [team, setTeam] = useState([]);
+  const [message, setMessage] = useState("");
+
+  const initialFighters = [
+  {
+    id: 1,
+    name: 'Survivor',
+    price: 12,
+    strength: 6,
+    agility: 4,
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/0c2d6b.png',
+  },
+  {
+    id: 2,
+    name: 'Scavenger',
+    price: 10,
+    strength: 5,
+    agility: 5,
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/033a16.png',
+  },
+  {
+    id: 3,
+    name: 'Shadow',
+    price: 18,
+    strength: 7,
+    agility: 8,
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/262c36.png',
+  },
+  {
+    id: 4,
+    name: 'Tracker',
+    price: 14,
+    strength: 7,
+    agility: 6,
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/3c1e70.png',
+  },
+  {
+    id: 5,
+    name: 'Sharpshooter',
+    price: 20,
+    strength: 6,
+    agility: 8,
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/4b2900.png',
+  },
+  {
+    id: 6,
+    name: 'Medic',
+    price: 15,
+    strength: 5,
+    agility: 7,
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/5a1e02.png',
+  },
+  {
+    id: 7,
+    name: 'Engineer',
+    price: 16,
+    strength: 6,
+    agility: 5,
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/5e103e.png',
+  },
+  {
+    id: 8,
+    name: 'Brawler',
+    price: 11,
+    strength: 8,
+    agility: 3,
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/67060c.png',
+  },
+  {
+    id: 9,
+    name: 'Infiltrator',
+    price: 17,
+    strength: 5,
+    agility: 9,
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/ac3220.png',
+  },
+  {
+    id: 10,
+    name: 'Leader',
+    price: 22,
+    strength: 7,
+    agility: 6,
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/e41f26.png',
+  },
+]
+
+  const [fighters, setFighters] = useState(initialFighters);
+
+  const handleAddFighter = (fighter) => {
+    if (money < fighter.price) {
+      setMessage(" Not enough money!");
+      return;
+    }
+
+    setMessage("");
+    setTeam([...team, fighter]);
+    setFighters(fighters.filter((fight) => fight.id !== fighter.id));
+    setMoney(money - fighter.price);
+  };
+
+  const handleRemoveFighter = (fighter) => {
+    setTeam(team.filter((fight) => fight.id !== fighter.id));
+    setFighters([...fighters, fighter]);
+    setMoney(money + fighter.price);
+  };
+
+  const totalStrength = team.reduce((sum, fight) => sum + fight.strength, 0);
+  const totalAgility = team.reduce((sum, fight) => sum + fight.agility, 0);
+
+  return (
+    <>
+      <h2>Money: ${money}</h2>
+
+      {message && <p >{message}</p>}
+
+      <h2>Available Fighters</h2>
+      {fighters.map((fighter) => (
+        <ul key={fighter.id}>
+          <li>
+            <img src={fighter.img} alt={fighter.name} />
+          </li>
+          <li>Name: {fighter.name}</li>
+          <li>Strength: {fighter.strength}</li>
+          <li>Agility: {fighter.agility}</li>
+          <li>Price: ${fighter.price}</li>
+          <button onClick={() => handleAddFighter(fighter)}>Add</button>
+        </ul>
+      ))}
+
+      <h2>Your Team</h2>
+      {team.length === 0 ? (
+        <p>Pick some team members</p>
+      ) : (
+        team.map((fighter) => (
+          <ul key={fighter.id}>
+            <li>
+              <img src={fighter.img} alt={fighter.name} />
+            </li>
+            <li>Name: {fighter.name}</li>
+            <li>Strength: {fighter.strength}</li>
+            <li>Agility: {fighter.agility}</li>
+            <li>Price: ${fighter.price}</li>
+            <button onClick={() => handleRemoveFighter(fighter)}>
+              Remove
+            </button>
+          </ul>
+        ))
+      )}
+
+      <h3>Total Strength: {totalStrength}</h3>
+      <h3>Total Agility: {totalAgility}</h3>
+    </>
+  );
+}
+
+export default App;
